eba1f53592feb7b53b3ed344224b7a397c0e4e4e
RonanMDONeill <ronan.oneill@ucdconnect.ie>
2018-04-09 01:37:17 +0100

    Update Weapons.java

--- Weapons.java
+++ Weapons.java
@@ -2,22 +2,22 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Weapons implements Iterable<Weapon>, Iterator<Weapon> {
-
+	
 	/*
-	 * Taken from Moodle page
+	 * Taken from Moodle page & edited
 	 */
 	
 	/*
 	 * TeamSynergy:
-	 * Dáire Murphy - 15441458
-	 * Rónan O'Neill - 16433656
+	 * DÃ¡ire Murphy - 15441458
+	 * RÃ³nan O'Neill - 16433656
 	 * Lorcan Rooney - 16413092
 	 */
-	
+
     private final ArrayList<Weapon> weapons = new ArrayList<>();
     private Iterator<Weapon> iterator;
 
-    Weapons(Map map) {
+    Weapons (Map map) {
         weapons.add(new Weapon(Names.WEAPON_NAMES[0],map.getRoom(Names.ROOM_NAMES[0])));
         weapons.add(new Weapon(Names.WEAPON_NAMES[1],map.getRoom(Names.ROOM_NAMES[1])));
         weapons.add(new Weapon(Names.WEAPON_NAMES[2],map.getRoom(Names.ROOM_NAMES[2])));
@@ -26,6 +26,17 @@ public class Weapons implements Iterable<Weapon>, Iterator<Weapon> {
         weapons.add(new Weapon(Names.WEAPON_NAMES[5],map.getRoom(Names.ROOM_NAMES[5])));
     }
 
+    // Checks to see if there is a weapon with said name
+    public boolean contains(String name) {
+        for (Weapon weapon : weapons) {
+            if (weapon.hasName(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // Gets weapon of said name, if available
     public Weapon get(String name) {
         for (Weapon weapon : weapons) {
             if (weapon.hasName(name)) {
@@ -34,17 +45,17 @@ public class Weapons implements Iterable<Weapon>, Iterator<Weapon> {
         }
         return null;
     }
-
+    
     public boolean hasNext() {
-        return iterator.hasNext();
+        return iterator.hasNext ();
     }
 
     public Weapon next() {
-        return iterator.next();
+        return iterator.next ();
     }
 
     public Iterator<Weapon> iterator() {
-        iterator = weapons.iterator();
+        iterator = weapons.iterator ();
         return iterator;
     }
 
