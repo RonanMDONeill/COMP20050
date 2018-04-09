1e2a1b98b963883b5fb15901e5c8c791d5621324
RonanMDONeill <ronan.oneill@ucdconnect.ie>
2018-04-09 01:33:39 +0100

    Update Cluedo.java

--- Cluedo.java
+++ Cluedo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Cluedo {
 	
 	/*
@@ -6,24 +8,34 @@ public class Cluedo {
 	
 	/*
 	 * TeamSynergy:
-	 * Dáire Murphy - 15441458
-	 * Rónan O'Neill - 16433656
+	 * DÃ¡ire Murphy - 15441458
+	 * RÃ³nan O'Neill - 16433656
 	 * Lorcan Rooney - 16413092
 	 */
 	
-
     private static final int MAX_NUM_PLAYERS = 6;
 
     private final Tokens tokens = new Tokens();
-    private Players players = new Players();
+    private final Players players = new Players();
     private final Dice dice = new Dice();
     private final Map map = new Map();
     private final Weapons weapons = new Weapons(map);
     private final UI ui = new UI(tokens,weapons);
-    private final Cards cards = new Cards();
-    private final Notes notes = new Notes();
-    private int playerNum;
-    private Player currPlayer;
+    private boolean moveOver;
+    private Player currentPlayer;
+    private Token currentToken;
+    private final Deck deck = new Deck();
+    private Cards cards = new Cards();
+    private ArrayList<Token> guessedTokens = new ArrayList<>();
+    private ArrayList<Weapon> guessedWeapons = new ArrayList<>();
+    private ArrayList<Room> guessedRooms = new ArrayList<>();
+    private ArrayList<String> responses = new ArrayList<>();
+    private Players removed = new Players();
+    private boolean eliminated = false;
+
+    private void announceTheGame() {
+        ui.displayMurderAnnouncement();
+    }
 
     private void inputPlayerNames() {
         int numPlayersSoFar = 0;
@@ -32,143 +44,329 @@ public class Cluedo {
             if (!ui.inputIsDone()) {
                 ui.inputToken(tokens);
                 Token token = tokens.get(ui.getTokenName());
-                players.add(new Player(ui.getPlayerName(),token, numPlayersSoFar));
+                players.add(new Player(ui.getPlayerName(),token));
                 token.setOwned();
                 numPlayersSoFar++;
             }
         } while (!ui.inputIsDone() && numPlayersSoFar<MAX_NUM_PLAYERS);
-        
-        // Global variable used in other methods
-        playerNum = numPlayersSoFar;
+    }
+
+    private void rollToStart() {
+        Players playersToRoll = new Players(players), playersWithHighRoll = new Players();
+        boolean tie = false;
+        do {
+            int highRoll = 0;
+            for (Player player : playersToRoll) {
+                dice.roll();
+                ui.displayDice(player,dice);
+                if (dice.getTotal() > highRoll) {
+                    tie = false;
+                    highRoll = dice.getTotal();
+                    playersWithHighRoll.clear();
+                    playersWithHighRoll.add(player);
+                } else if (dice.getTotal() == highRoll) {
+                    tie = true;
+                    playersWithHighRoll.add(player);
+                }
+            }
+            if (tie) {
+                ui.displayRollDraw();
+                playersToRoll = new Players(playersWithHighRoll);
+                playersWithHighRoll.clear();
+            }
+        } while (tie);
+        players.setCurrentPlayer(playersWithHighRoll.get(0).getName());
+        ui.displayRollWinner(players.getCurrentPlayer());
+        ui.display();
+    }
+
+    private void dealCards() {
+        deck.selectMurderCards();
+        deck.prepareToDeal(players.getNumberOfPlayers());
+        for (Player player : players) {
+            player.addCards(deck.dealCards());
+        }
+        ui.displayCardsDealt();
+        cards = deck.allCardsCopy;
+    }
+
+    private void roll() {
+        if (!moveOver) {
+            dice.roll();
+            ui.displayDice(currentPlayer, dice);
+            int squaresMoved = 0;
+            if (currentToken.isInRoom()) {
+                if (currentToken.getRoom().getNumberOfDoors()>1) {
+                    boolean exitDone = false;
+                    do {
+                        ui.inputDoor(currentPlayer);
+                        if (ui.getDoor()>= 1 || ui.getDoor()<=currentToken.getRoom().getNumberOfDoors()) {
+                            currentToken.leaveRoom(ui.getDoor()-1);
+                            exitDone = true;
+                        } else {
+                            ui.displayErrorNotADoor();
+
+                        }
+                    } while (!exitDone);
+                } else {
+                    currentToken.leaveRoom();
+                }
+                ui.display();
+            }
+            do {
+                ui.inputMove(currentPlayer, squaresMoved+1, dice.getTotal());
+                Coordinates currentPosition = currentToken.getPosition();
+                Coordinates newPosition;
+                if (map.isValidMove(currentPosition, ui.getMove())) {
+                    newPosition = map.getNewPosition(currentPosition, ui.getMove());
+                    if (map.isDoor(currentPosition, newPosition)) {
+                        Room room = map.getRoom(newPosition);
+                        currentToken.enterRoom(room);
+                    } else {
+                        currentToken.setPosition(newPosition);
+                    }
+                    squaresMoved++;
+                    if (squaresMoved==dice.getTotal() || currentPlayer.getToken().isInRoom()) {
+                        moveOver = true;
+                    }
+                    ui.display();
+                } else {
+                    ui.displayErrorInvalidMove();
+                }
+            } while (!moveOver);
+        } else {
+            ui.displayErrorAlreadyMoved();
+        }
+    }
+
+    private void passage() {
+        if (!moveOver) {
+            if (currentToken.isInRoom() && currentToken.getRoom().hasPassage()) {
+                Room destination = currentToken.getRoom().getPassageDestination();
+                currentToken.leaveRoom();
+                currentToken.enterRoom(destination);
+                moveOver = true;
+                ui.display();
+            } else {
+                ui.displayErrorNoPassage();
+            }
+        } else {
+            ui.displayErrorAlreadyMoved();
+        }
     }
     
-    // Call dealCards method from Cards class
-    private void setCards() {
-    	cards.dealCards(playerNum, players);
+    // Method for questioning
+    private void question(boolean hasGuessed) {
+    	int count = players.getNumberOfPlayers() - 1;
+    	boolean disproved = false;
+    	boolean hasToken = false, hasWeapon = false, hasRoom = false;
+    	Players guessList = new Players(players);
+    	Player questionedPlayer = null;
+    	Room room = null;
+    	Token token = null;
+    	Weapon weapon = null;
+    	String disproveCard = "", inCellar = "cellar";
+
+    	
+    	if(currentToken.isInRoom() && !hasGuessed && !inCellar.equals(currentToken.getRoom().toString().trim().toLowerCase())) {
+    		// Get room coordinates and guess
+    		room = currentToken.getRoom();
+    		guessedRooms.add(room);
+    		ui.guessToken(tokens);
+    		token = tokens.get(ui.getTokenName());
+    		guessedTokens.add(token);
+    		ui.guessWeapon(weapons);
+    		weapon = weapons.get(ui.getWeaponName());
+    		guessedWeapons.add(weapon);
+    		
+    		// Move token and weapon to guess room
+    		Coordinates addWeapon = room.addItem();
+    		token.enterRoom(room);
+    		weapon.setPosition(addWeapon);
+    		ui.display();
+    	
+    		guessList.setCurrentPlayer(currentPlayer.getName());
+    	
+    		while(!disproved && count > 0) {
+    			// Next player
+    			guessList.turnOver();
+    			questionedPlayer = guessList.getCurrentPlayer();
+    		
+    			// Clear infoPanel
+    			ui.clear();
+    		
+    			// Confirm switch
+    			ui.confirmSwitch(questionedPlayer);
+    		
+    			// Display guess
+    			ui.displayGuess(currentPlayer, token, weapon, room);
+    		
+    			// Check if player has a card from the guess
+    			hasToken = questionedPlayer.hasCard(token.getName());
+    			hasWeapon = questionedPlayer.hasCard(weapon.getName());
+    			hasRoom = questionedPlayer.hasCard(room.toString());
+    		
+    			// Show player their notes
+    			ui.displayNotes(questionedPlayer, deck);
+    		
+    			// Get response from player
+    			disproved = ui.disproveGuess(token, weapon, room, hasToken, hasWeapon, hasRoom);
+    			disproveCard = ui.getDisproveCard();
+    			
+    			count--;
+    		}
+    	
+    		// Clear infoPanel
+    		ui.clear();
+    	
+    		// If disproved, mark in notes
+    		if(disproved) {
+    			Card card = cards.getCard(disproveCard);
+    			Cards updated = new Cards();
+    			updated = currentPlayer.getCards();
+    			updated.add(card);
+    			currentPlayer.addCards(updated);
+    			responses.add(questionedPlayer.getName() + " disproved this with the " + disproveCard + " card");
+    		
+    			ui.displayDisprove(questionedPlayer, disproveCard);
+    		}
+    	
+    		// If not disproved
+    		if(count == 0 && !disproved) {
+    			responses.add("fail");
+    			ui.displayNoDisprove();
+    		}
+    	}
+    	// If player is not in room
+    	else if(!currentToken.isInRoom()){
+    		ui.displayErrorNotInRoom();
+    	}
+    	// If already guessed this turn
+    	else if(hasGuessed){
+    		ui.displayErrorHasGuessed();
+    	}
+    	// If player is in cellar
+    	else {
+    		ui.displayErrorCellarGuess();
+    	}
     }
     
-    // Method that determines who goes first based on dice rolls
-    public void decideStarter () {
-		Players inPlayers = new Players(players), selectedPlayers = new Players();
-		boolean tie = false;
-		do {
-			int highestTotal = 0;
-			for (Player p : inPlayers.get()) {
-				dice.roll();
-				ui.displayDice(p,dice);
-				if (dice.getTotal() > highestTotal) {
-					tie = false;
-					highestTotal = dice.getTotal();
-					selectedPlayers.clear();
-					selectedPlayers.add(p);
-				} else if (dice.getTotal() == highestTotal) {
-					tie = true;
-					selectedPlayers.add(p);
-				}
-			}
-			if (tie) {
-				ui.displayRollDraw();
-				inPlayers = new Players(selectedPlayers);
-				selectedPlayers.clear();
-			}
-		} while (tie);
-		// Set the new starter
-		players.setCurrentPlayer(selectedPlayers.get(0).getName(), playerNum);
-		currPlayer = selectedPlayers.get(0);
-		ui.displayRollWinner(currPlayer);
-		ui.display();
-		return;
-	}
+    // Method that displays all previous questions and their responses
+    private void log() {
+    	ui.displayLog(guessedTokens, guessedWeapons, guessedRooms, responses);
+    }
+    
+    // Method for accusing
+    private boolean accuse() {
+    	String token, weapon, room;
+    	boolean tokenBool, weaponBool, roomBool;
+    	Cards murderCards = new Cards();
+    	String inCellar = "cellar";
+    	
+    	boolean correct = false;
+    	
+    	if(currentToken.isInRoom() && inCellar.equals(currentToken.getRoom().toString().trim().toLowerCase())) {
+    		// Get accusation
+    		ui.clear();
+    		ui.displayAccusation();
+    		token = ui.accuseToken(tokens);
+    		weapon = ui.accuseWeapon(weapons);
+    		room = ui.accuseRoom();
+    		
+    		// Get murder cards
+    		murderCards = deck.getMurderCards();
+    		
+    		// Check if accusation is correct
+    		tokenBool = murderCards.contains(token);
+    		weaponBool = murderCards.contains(weapon);
+    		roomBool = murderCards.contains(room);
+    		
+    		// Player was correct
+    		if(tokenBool && weaponBool && roomBool) {
+    			ui.displayGameWinner(currentPlayer);
+    			ui.displaySolution(deck.getMurderCards());
+    			correct = true;
+    		}
+    		
+    		// Player was incorrect
+    		else {
+    			ui.displayElimination();
+    			correct = false;
+    			
+    			// Remove player from game and add to removed players list
+        		int index = players.getIndex();
+        		removed.add(players.removePlayer(index));
+        		eliminated = true;
+    		}
+    		
+    	}
+    	
+    	// Must be in cellar to accuse
+    	else {
+    		ui.displayErrorNotInCellar();
+    	}
+    	
+    	return correct;
+    }
 
     private void takeTurns() {
-        boolean moveOver, turnOver, gameOver = false;
+        boolean turnOver, gameOver = false, hasGuessed = false, correct = false;
         do {
             turnOver = false;
             moveOver = false;
             do {
-            	Player currentPlayer = players.getCurrentPlayer();
-                Token currentToken = currentPlayer.getToken();
+                currentPlayer = players.getCurrentPlayer();
+                currentToken = currentPlayer.getToken();
+                ui.displayEliminatedPlayers(removed, eliminated);
                 ui.inputCommand(currentPlayer);
                 switch (ui.getCommand()) {
                     case "roll": {
-                        if (!moveOver) {
-                            dice.roll();
-                            ui.displayDice(currentPlayer, dice);
-                            int squaresMoved = 0;
-                            if (currentToken.isInRoom()) {
-                                if (currentToken.getRoom().getNumberOfDoors()>1) {
-                                    boolean exitDone = false;
-                                    do {
-                                        ui.inputDoor(currentPlayer);
-                                        if (ui.getDoor()>= 1 || ui.getDoor()<=currentToken.getRoom().getNumberOfDoors()) {
-                                            currentToken.leaveRoom(ui.getDoor()-1);
-                                            exitDone = true;
-                                        } else {
-                                            ui.displayErrorNotADoor();
-                                        }
-                                    } while (!exitDone);
-                                } else {
-                                    currentToken.leaveRoom();
-                                }
-                                ui.display();
-                            }
-                            do {
-                                ui.inputMove(currentPlayer, squaresMoved+1, dice.getTotal());
-                                Coordinates currentPosition = currentToken.getPosition();
-                                Coordinates newPosition;
-                                if (map.isValidMove(currentPosition, ui.getMove())) {
-                                    newPosition = map.getNewPosition(currentPosition, ui.getMove());
-                                    if (map.isDoor(currentPosition, newPosition)) {
-                                        Room room = map.getRoom(newPosition);
-                                        currentToken.enterRoom(room);
-                                    } else {
-                                        currentToken.setPosition(newPosition);
-                                    }
-                                    squaresMoved++;
-                                    if (squaresMoved==dice.getTotal() || currentPlayer.getToken().isInRoom()) {
-                                        moveOver = true;
-                                    }
-                                    ui.display();
-                                } else {
-                                    ui.displayErrorInvalidMove();
-                                }
-                            } while (!moveOver);
-                        } else {
-                            ui.displayErrorAlreadyMoved();
-                        }
+                        roll();
                         break;
                     }
                     case "passage": {
-                        if (!moveOver) {
-                            if (currentToken.isInRoom() && currentToken.getRoom().hasPassage()) {
-                                Room destination = currentToken.getRoom().getPassageDestination();
-                                currentToken.leaveRoom();
-                                currentToken.enterRoom(destination);
-                                moveOver = true;
-                                ui.display();
-                            } else {
-                                ui.displayErrorNoPassage();
-                            }
-                        } else {
-                            ui.displayErrorAlreadyMoved();
-                        }
+                        passage();
                         break;
                     }
-                    // Notes command that displays all the cards
+                    case "question":{
+                    	question(hasGuessed);
+                    	hasGuessed = true;
+                    	break;
+                    }
+                    case "log":{
+                    	log();
+                    	break;
+                    }
+                    case "accuse":{
+                    	correct = accuse();
+                    	if(correct) {
+                    		turnOver = true;
+                    		gameOver = true;
+                    	}
+                    	else {
+                    		turnOver = true;
+                    	}
+                    	// If only one player left, they win
+                    	if(players.getNumberOfPlayers() == 1) {
+                    		ui.displayGameWinner(players.get(0));
+                    		ui.displaySolution(deck.getMurderCards());
+                    		turnOver = true;
+                    		gameOver = true;
+                    	}
+                    	
+                    	break;
+                    }
                     case "notes": {
-                    	// Characters
-                    	ui.displayString("Notes: " + notes.displayNotes(cards, players));
-                    	break;
+                        ui.displayNotes(currentPlayer, deck);
+                        break;
                     }
-                    // Cheat command to show the cards in the envelope
-                    case "cheat": {
-                    	ui.displayString("Murder Envelope:\nPerson: " + cards.getPerson() + "\nWeapon: " + cards.getWeapon() + "\nRoom: " + cards.getRoom());
-                    	break;
+                    case "cheat" : {
+                        ui.displaySolution(deck.getMurderCards());
+                        break;
                     }
-                    // Help command that displays help for the game
-                    case "help": {
-                    	ui.displayCommandHelp();
-                    	break;
+                    case "help" : {
+                        ui.displayHelp();
+                        break;
                     }
                     case "done": {
                         turnOver = true;
@@ -177,22 +375,25 @@ public class Cluedo {
                     case "quit": {
                         turnOver = true;
                         gameOver = true;
+                        System.exit(0);
                         break;
                     }
                 }
             } while (!turnOver);
             if (!gameOver) {
                 players.turnOver();
+            	ui.clear();
+            	hasGuessed = false;
             }
         } while (!gameOver);
     }
 
     public static void main(String[] args) {
         Cluedo game = new Cluedo();
+        game.announceTheGame();
         game.inputPlayerNames();
-        game.decideStarter();
-        game.setCards();
+        game.dealCards();
+        game.rollToStart();
         game.takeTurns();
-        System.exit(0);
     }
 }
